90f6371aacf6b2e23601baa5e6b41ccb21c190fd
root <root@GI01WEB500.integrado.int>
2023-07-24 23:59:40 -0300

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,9 @@
+const newrelic = require('newrelic');
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import newrelic from 'newrelic';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NewrelicInterceptor } from './newrelic.interceptor';
+
+
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new NewrelicInterceptor());
+  await app.listen(process.env.PORT || 3001);
+}
+bootstrap();
